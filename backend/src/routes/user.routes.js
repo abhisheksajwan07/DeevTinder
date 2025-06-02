9bc52fb28020e91cbc192bc92bdf42c690592b93
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-06-02 17:09:38 +0530

    feed api done

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth.middlewares");
 const ConnectionRequest = require("../models/connectionRequest.model");
+const User = require("../models/user.model");
 const { connect } = require("mongoose");
 const USER_SAFE_DATA = "firstName lastName photoUrl age about gender skills";
 userRouter.get("/user/requests", userAuth, async (req, res) => {
@@ -51,8 +52,33 @@ userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     //User should see all the user cards except
     // his own Card,his connection,ignored people,connection request
-    
-  } catch (err) {}
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    const hideUserFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUserFeed.add(req.fromUserId.toString());
+      hideUserFeed.add(req.toUserId.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 module.exports = userRouter;
 
