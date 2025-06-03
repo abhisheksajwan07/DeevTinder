bd17f3985a60cb45656e40a218a613b3358dd23b
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-06-03 21:37:20 +0530

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,50 +3,12 @@ require("dotenv").config();
 const connectDB = require("./config/database.js");
 
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+app.use(cors());
 app.use(express.json()); //whenver im reading the req.,
 app.use(cookieParser());
 //i want that req's data to be passed into json data
-
-// app.get("/feed", async (req, res) => {
-//   try {
-//     const users = await User.find({});
-//     if (users.length === 0) {
-//       res.send("No user found");
-//     } else {
-//       console.log(users);
-//       res.send(users);
-//     }
-//   } catch (err) {
-//     res.status(400).send("Something went wrong");
-//   }
-// });
-// app.get("/user", async (req, res) => {
-//   //getting user from body
-//   const userEmail = req.body.emailId;
-//   try {
-//     const users = await User.findOne({ emailId: userEmail });
-//     if (users.length === 0) {
-//       res.status(400).send("User not found");
-//     } else {
-//       // console.log(users)
-//       res.send(users);
-//     }
-//   } catch (err) {
-//     res.status(400).send("Something went wrong");
-//   }
-// });
-// app.delete("/user", async (req, res) => {
-//   const userId = req.body.userId;
-
-//   try {
-//     const users = await User.findByIdAndDelete(userId);
-//     res.send("User deleted Successfully");
-//   } catch (err) {
-//     res.status(400).send("Something went wrong");
-//   }
-// });
 
 const authRouter = require("./routes/auth.routes.js");
 const profileRouter = require("./routes/profile.routes.js");
