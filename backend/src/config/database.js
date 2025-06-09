876c3f42b6139d384f0d27deb7552b2b1f09016a
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-06-09 13:50:37 +0530

    minor change

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+
 const connectDB = async () => {
+  console.log("🌐 Attempting DB connection to:", process.env.MONGO_URI);
+
   try {
-   
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+     
+    });
     console.log("✅ MongoDB Connected");
+
+    mongoose.connection.on("error", (err) => {
+      console.error("❌ Mongoose runtime error:", err);
+    });
   } catch (err) {
     console.error("❌ MongoDB Connection Failed:", err.message);
   }
